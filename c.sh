627173cf32e29c3aa5c4a558a700e899a51e0131
MrBSTRD13 <55816685+MrBSTRD13@users.noreply.github.com>
2019-10-19 04:52:31 +0000

    Add files via upload

---
+++ c.sh
@@ -0,0 +1,385 @@
+r='\033[31;1m' #merah
+b='\033[34;1m' #biru
+cy='\033[36;1m' #cyan
+w='\033[37;1m' #puti
+u='\033[33;95m' #ungu
+y='\033[33;1m' #kuning
+g='\033[32;1m' #hijau
+
+
+clear
+cd /$HOME/Spam
+sleep 3
+echo $cy"Pilih No.1 Untuk download passwordnya"
+echo $g"[1] Unduh Password"
+echo $g"[2] Masuk Toolsnya"
+read -p "Masukkan Pilihan : " pil
+if [ $pil = 1 ]
+then
+sleep 2
+echo $g"Okeh Langsung Aja Kita Download Passwordnya"
+sleep 2
+echo $g"Sabar Sat Sedang Membuka Browser ... "
+sleep 5
+xdg-open https://realsht.mobi/pVPua
+fi
+if [ $pil = 2 ]
+then
+clear
+echo $g"Sudah Punya password?"
+sleep 2
+echo $g"Silahkan Login"
+sleep 2
+fi
+read -p "Password : " pass
+if [ $pass = "FCKJRX" ]
+then
+clear
+sleep 1
+read -p "masukan nama elu disini : " nama
+echo "selamat datang" $nama | lolcat
+sleep 2
+echo "yang ganteng" | lolcat
+sleep 2
+echo "yang pro" | lolcat
+sleep 2
+echo "yang baik" | lolcat
+sleep 2
+echo "yang bijak" | lolcat
+sleep 2
+echo "yang pinter" | lolcat
+sleep 2
+echo "yang sukanya coli sama ngeue" | lolcat
+sleep 2
+echo "saudara $nama jangan lupa follow ig admin ya namanya agung_prasetyo_13" | lolcat
+sleep 3
+echo "saudara $nama jangan direcode ya bangsat scripnya" | lolcat
+sleep 3
+echo "gw cape sat buatnya" | lolcat
+sleep 3
+echo "recode tydak akan membuatmu pro " | lolcat
+sleep 3
+clear
+sleep 1
+toilet -f slant --gay "Success"
+sleep 1
+clear
+xdg-open https://www.instagram.com/agung_prasetyo_13/
+else
+echo 'gblk slh sat:v'  | lolcat
+sleep 3
+echo " sabar sat lagi membuka browser  ... " | lolcat
+sleep 3
+echo "untuk download pass nya" | lolcat
+sleep 3
+echo "wait ... !!!" | lolcat
+sleep 5
+xdg-open https://realsht.mobi/pVPua
+exit
+sleep 2
+fi
+clear
+figlet SABAR SAT...!!! | lolcat
+sleep 2
+clear
+figlet 1 | lolcat
+sleep 3
+clear
+figlet 2 | lolcat
+sleep 3
+clear
+figlet 3 | lolcat
+sleep 3
+clear
+sleep 3
+echo $u"   ▄████████    ▄███████▄    ▄████████     ▄▄▄▄███▄▄▄▄"
+echo $u"   ███    ███   ███    ███   ███    ███ ▄██▀▀▀███▀▀▀██▄"
+echo $u"   ███    █▀    ███    ███   ███    ███ ███   ███   ███"
+echo $y"   ███          ███    ███   ███    ███ ███   ███   ███"
+echo $y" ▀███████████ ▀█████████▀  ▀███████████ ███   ███   ███"
+echo $y"          ███   ███          ███    ███ ███   ███   ███"
+echo $cy"    ▄█    ███   ███          ███    ███ ███   ███   ███"
+echo $cy"   ▄████████▀  ▄████▀        ███    █▀   ▀█   ███   █▀"
+echo
+echo
+echo "-----------------------------------------------------------" | lolcat
+sleep 2
+echo $y"["$w"+"$y"]"$b" Author  : MrBSTRD13"
+echo $y"["$w"+"$y"]"$b" GitHub  : https://github.com/MrBSTRD13"
+echo $y"["$w"+"$y"]"$b" Contact : 083862170349"
+echo $y"["$w"+"$y"]"$b" TOOLS   : SPAM CALL & SMS"
+sleep 0.1
+echo "-----------------------------------------------------------" | lolcat
+sleep 0.1
+echo
+echo "[1]  BRUTAL SMS"
+echo "========================" | lolcat
+echo "[2]  SPAM-SMS"
+echo "========================" | lolcat
+echo "[3]  LITE-SPAM"
+echo "========================" | lolcat
+echo "[4]  GCO-SPAM"
+echo "========================" | lolcat
+echo "[5]  SPAMMER GRAB"
+echo "========================" | lolcat
+echo "[6]  BOM-SMS"
+echo "========================" | lolcat
+echo "[7]  BOM-ATTACK"
+echo "========================" | lolcat
+echo "[8]  T-BOMB"
+echo "========================" | lolcat
+echo "[9]  SPAM-TRI"
+echo "========================" | lolcat
+echo "[10] SPAZ-SMS"
+echo "========================" | lolcat
+echo "[11] SPAM-CALL"
+echo "========================" | lolcat
+echo "[12] SPAM-CALL V2"
+echo "========================" | lolcat
+echo "[13] ENOXUIA-SPAM"
+echo "========================" | lolcat
+echo "[14] SMS-ZPAM"
+echo "========================" | lolcat
+echo "[15] SPAZ-CALL"
+echo "========================" | lolcat
+echo "[16] SPAM-TROLLPROJECT"
+echo "========================" | lolcat
+echo "[17] MULTI-SPAM"
+echo "========================" | lolcat
+echo "[18] SpamCallz"
+echo "========================" | lolcat
+echo "[19] SPAMMER-Z"
+echo "========================" | lolcat
+echo "[20] SPAM-TERMUX ART"
+echo "========================" | lolcat
+echo 
+echo $g" ╭─"$r"Masukan Pilihan satt"
+read -p " ╰─────────MrBSTRD13>> " pil
+if [ $pil = 1 ]
+then
+clear
+sleep 3
+figlet WAIT SAT...!!! | lolcat
+git clone https://github.com/TERMUXID3/brutal-sms
+cd brutal-sms
+chmod 777 run.py
+python2 run.py
+exit
+fi
+if [ $pil = 2 ]
+then
+clear
+sleep 3
+figlet WAIT SAT...!!! | lolcat
+pkg install php -y
+git clone https://github.com/KANG-NEWBIE/SpamSms
+cd SpamSms
+chmod +x *
+php sms.php
+exit
+fi
+if [ $pil = 3 ]
+then
+clear
+sleep 3
+figlet WAIT SAT...!!! | lolcat
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+chmod +x *
+sh LITESPAM.sh
+exit
+fi
+if [ $pil = 4 ]
+then
+clear
+sleep 3
+figlet WAIT SAT...!!! | lolcat
+git clone https://github.com/Amriez/gcospam
+cd gcospam
+chmod +x *
+sh gco.sh
+exit
+fi
+if [ $pil = 5 ]
+then
+clear
+sleep 3
+figlet WAIT SAT...!!! | lolcat
+git clone http://github.com/p4kl0nc4t/Spammer-Grab
+cd Spammer-Grab
+echo $cy"..:::["$w"ROOT®IQBALMH18"$cy"]:::.."
+echo "Masukan Nomor dengan ( 62 ) Contoh :"$r" 62877XXXXXXXX"
+read -p"Nomor Target :" no
+python2 spammer.py $no --delay 30
+exit
+fi
+if [ $pil = 6 ]
+then
+clear
+sleep 3
+figlet WAIT SAT...!!! | lolcat
+pkg install git php -y
+git clone https://github.com/JeelsBoobz/BomSMS
+cd BomSMS
+chmod +x *
+php BomSMS.php
+exit
+fi
+if [ $pil = 7 ]
+then
+clear
+sleep 3
+figlet WAIT SAT...!!! | lolcat
+pkg install python git
+git clone https://github.com/Gameye98/santet-online
+cd santet-online
+chmod +x *
+python santet.py
+exit
+fi
+if [ $pil = 8 ]
+then
+clear
+sleep 3
+figlet WAIT SAT...!!! | lolcat
+git clone https://github.com/TheSpeedX/TBomb.git
+cd TBomb
+chmod +x *
+./TBomb.sh
+exit
+fi
+if [ $pil = 9 ]
+then
+clear
+sleep 3
+figlet WAIT SAT...!!! | lolcat
+git clone https://github.com/hatakecnk/SPAMTRI2
+cd SPAMTRI2
+sh spam-tri.sh
+exit
+fi
+if [ $pil = 10 ]
+then
+clear
+sleep 3
+figlet WAIT SAT...!!! | lolcat
+git clone https://github.com/Gameye98/SpazSMS
+cd SpazSMS
+python2 lite.py
+echo $cy"..:::["$w"ROOT®IQBALMH18"$cy"]:::.."
+echo "Masukan Nomor dengan ( 62 ) Contoh :"$r" 62877XXXXXXXX"
+read -p"Nomor Target :" no
+python2.7 lite.py --sms $no
+exit
+fi
+if [ $pil = 11 ]
+then
+clear
+figlet WAIT SAT...!!! | lolcat
+cd /$HOME/SPAMMER/DIR
+git clone https://github.com/storiku/Call
+cd Call
+php spamcall.php
+exit
+fi
+if [ $pil = 12 ]
+then
+clear
+sleep 3
+figlet WAIT SAT...!!! | lolcat
+git clone https://github.com/Aditya021/SpamCall
+cd SpamCall
+php SpamCall.php
+exit
+fi
+if [ $pil = 13 ]
+then
+clear
+sleep 3
+figlet WAIT SAT...!!! | lolcat
+git clone https://github.com/underxploit/Enoxuia
+cd Enoxuia
+chmod +x *
+php enox.php
+exit
+fi
+if [ $pil = 14 ]
+then
+clear
+sleep 3
+figlet WAIT SAT...!!!| lolcat
+pkg install git python3
+pip2 install mechanize requests
+git clone https://github.com/Zen-Clay/sms-spamz
+cd sms-spamz
+python3 zpam.py
+exit
+fi
+if [ $pil = 15 ]
+then
+clear
+sleep 3
+figlet WAIT SAT...!!! | lolcat
+git clone https://github.com/Gameye98/SpazSMS
+cd SpazSMS
+python2 lite.py
+echo $r"..:::["$w"ROOT®IQBALMH18"$r"]:::.."
+echo "Masukan Nomor dengan ( 62 ) Contoh :"$r" 62877XXXXXX"
+read -p"Nomor Target :" no
+python2.7 lite.py --call $no
+exit
+fi
+if [ $pil =  16 ]
+then
+clear
+sleep 3
+figlet WAIT SAT...!!! | lolcat
+git clone https://github.com/zlucifer/troll_project
+cd troll_project
+chmod +x *
+sh troll.sh
+exit
+fi
+if [ $pil = 17 ]
+then
+clear
+figlet WAIT SAT...!!! | lolcat
+cd /$HOME/SPAMMER/DIR
+git clone https://github.com/LOoLzeC/CALL-BRUTAL
+cd CALL-BRUTAL
+chmod +x *
+python2 call.py
+exit
+fi
+if [ $pil = 18 ]
+then
+clear
+sleep 3
+figlet WAIT SAT...!!! | lolcat
+pkg install python3
+pip2 install mechanize requests
+git clone https://github.com/Zen-Clay/spamcallz
+cd spamcallz
+python3 SpamCallz.py
+exit
+fi
+if [ $pil = 19 ]
+then
+clear
+figlet WAIT SAT ... !!! | lolcat
+sleep 3
+git clone https://github.com/rezadkim/spammerZ
+cd spammerZ
+python2 spam.py
+exit
+fi
+if [ $pil = 20 ]
+then
+clear
+figlet WAIT SAT...!!! | lolcat
+sleep 3
+git clone https://github.com/TermuxArt/Spammer
+cd Spammer
+python spam.py
+exit
+fi
