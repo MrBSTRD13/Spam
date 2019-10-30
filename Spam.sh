a5a198a74436550ba547006706ce5226873f72ed
MrBSTRD13 <55816685+MrBSTRD13@users.noreply.github.com>
2019-10-30 02:27:53 +0000

    Add files via upload

---
+++ Spam.sh
@@ -0,0 +1,3 @@
+z="
+";Gz='E/Sp';Oz='Bomc';Qz='php';Kz='logi';Jz='on2 ';Lz='n.py';Dz='p 2';Mz='c';Az='clea';Nz='php ';Fz='$HOM';Hz='am';Ez='cd /';Pz='all.';Bz='r';Cz='slee';Iz='pyth';
+eval "$Az$Bz$z$Cz$Dz$z$Ez$Fz$Gz$Hz$z$Iz$Jz$Kz$Lz$Mz$z$Az$Bz$z$Cz$Dz$z$Ez$Fz$Gz$Hz$z$Nz$Oz$Pz$Qz"
